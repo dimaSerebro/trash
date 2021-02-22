96c9de8d23b662b908716b2832ce54d6e7ebfdea
Din <dima.agstivjobs@gmail.com>
2021-02-22 20:01:35 +0200

    First

---
+++ js/script.js
@@ -0,0 +1,28 @@
+            // Простое общение с пользователем
+"use strict";
+
+
+/* const result = confirm("Are you here?");   
+console.log(result); */
+
+
+/* const age = +prompt("You are 18 years old?", "10");
+console.log(age + 5); */
+
+/* const answers = [];
+
+answers[0] = prompt("Как ваше Имя?", "");
+answers[1] = prompt("Как ваше Фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+document.write(answers); */
+
+
+  //Урок 008 Интерполяция
+const category = 'toys';
+console.log(`https://google.com/${category}/5`);
+// если мы хотим сложить две строки оборачиваем косыми скобками `https://google.com/ пишем знак $ и оборачиваем {нашу переменную} `
+
+
+let user = 'Ivan';
+alert(`Hello, ${user}`);
